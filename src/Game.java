8b254a545b11e736efe60c30b4d593384554f14a
rushamdr0410 <rushamanandhar3@gmail.com>
2026-07-08 20:47:41 +0545

    GUI alignment

--- src/Game.java
+++ src/Game.java
@@ -7,6 +7,7 @@ public class Game {
     private int secretNumber;
     private int attempts;
     private Random random;
+    private int range;
 
     private JLabel resultLabel;
     private JTextField guessText;
@@ -20,17 +21,28 @@ public class Game {
         frame.setLayout(new GridLayout(1,1)); //BorderLayout or GridLayout(rows, cols) or FlowLayout
 
         JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
         panel.setVisible(true);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout());
+        inputPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JLabel titleLabel =  new JLabel("CloudExify Guessing Game");
         titleLabel.setFont(new Font(titleLabel.getFont().getFontName(),Font.BOLD,16));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         resultLabel = new JLabel("");
+        resultLabel.setFont(new Font(resultLabel.getFont().getFontName(),Font.BOLD,16));
+        resultLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         JLabel messageLabel = new JLabel("Please enter your guess: ");
         messageLabel.setFont(new Font(messageLabel.getFont().getFontName(),Font.BOLD,12));
         guessText = new JTextField();
-        guessText.setColumns(10);
+        guessText.setPreferredSize(new Dimension(100,30));
+        guessText.setHorizontalAlignment(JTextField.CENTER);
+        guessText.setMargin(new Insets(1, 5, 1, 5));
         JButton guessButton = new JButton("Guess");
         guessButton.setFont(new Font(guessButton.getFont().getFontName(),Font.BOLD,12));
+        guessButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         guessButton.addActionListener(e -> {
             String txt = guessText.getText();
             try{
@@ -38,6 +50,7 @@ public class Game {
                 checkGuess(guess);
 
             }catch(NumberFormatException ex){
+                resultLabel.setForeground(Color.RED);
                 resultLabel.setText("Invalid Guess!");
             }
 
@@ -45,9 +58,17 @@ public class Game {
 
         panel.add(titleLabel);
         panel.add(resultLabel);
-        panel.add(messageLabel);
-        panel.add(guessText);
+
+        inputPanel.add(messageLabel);
+        inputPanel.add(guessText);
+        inputPanel.setMaximumSize(inputPanel.getPreferredSize());
+        panel.add(inputPanel);
+        panel.add(Box.createRigidArea(new Dimension(0,10)));
+
         panel.add(guessButton);
+
+
+        panel.add(Box.createVerticalGlue());
 
         frame.add(panel);
 
@@ -65,13 +86,21 @@ public class Game {
 
     private void checkGuess(int guess){
         attempts++;
+//        if(guess <1 || guess > 100){
+//            resultLabel.setForeground(Color.RED);
+//            resultLabel.setText("Guessing Range is between 1-100");
+//            return;
+//        }
         if(guess < secretNumber){
-            resultLabel.setText("Higher");
+            resultLabel.setForeground(Color.BLUE);
+            resultLabel.setText("HIGHER~");
         }
         else if(guess > secretNumber){
-            resultLabel.setText("Lower");
+            resultLabel.setForeground(Color.BLUE);
+            resultLabel.setText("LOWER~");
         }
         else{
+            resultLabel.setForeground(Color.GREEN);
             resultLabel.setText("Yay! You Guessed it!");
         }
 
